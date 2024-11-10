7e6eb64ce8f7b956757cf57ba64403a99556b948
sams0899 <samuel.antoinne99@gmail.com>
2024-11-10 12:41:58 -0500

    task 9

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -11,18 +11,72 @@ const isValid = (username)=>{ //returns boolean
 
 const authenticatedUser = (username,password)=>{ //returns boolean
 //write code to check if username and password match the one we have in records.
+// Filter the users array for any user with the same username and password
+    let validusers = users.filter((user) => {
+        return (user.username === username && user.password === password);
+    });
+    // Return true if any valid user is found, otherwise false
+    if (validusers.length > 0) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  const username = req.body.username;
+    const password = req.body.password;
+    // Check if username or password is missing
+    if (!username || !password) {
+        return res.status(404).json({ message: "Error logging in" });
+    }
+    // Authenticate user
+    if (authenticatedUser(username, password)) {
+        // Generate JWT access token
+        let accessToken = jwt.sign({
+            data: password
+        }, 'access', { expiresIn: 60 * 60 });
+        // Store access token and username in session
+        req.session.authorization = {
+            accessToken, username
+        }
+        return res.status(200).send("User successfully logged in");
+    } else {
+        return res.status(208).json({ message: "Invalid Login. Check username and password" });
+    }
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  // Extract isbn parameter from request URL
+    const isbn = req.params.isbn;
+    const username = req.session.authorization?.username;;
+    let filtered_books = books[isbn]; 
+    if (filtered_books) {  
+        let reviews = req.body.reviews;
+        if (reviews) {
+            filtered_books["reviews"] = `${reviews} - This review is by ${username}`;
+        }
+        books[isbn] = filtered_books; 
+        res.send(`Book with ISBN ${isbn} updated.`);
+    } else {
+        res.send("Unable to find book!");
+    }
+});
+
+// Add a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    // Extract isbn parameter from request URL
+    const isbn = req.params.isbn;
+    if (isbn) {
+        // Delete friend from 'friends' object based on provided email
+        delete books[isbn].reviews;
+    }
+    
+    // Send response confirming deletion of friend
+    res.send(`Book's review with the isbn ${isbn} deleted.`);
 });
 
 module.exports.authenticated = regd_users;
